f14b22baa5c3887c6e63d5997cb0577f5e5319dd
ypeeters <developer@kindengezin.be>
2017-11-15 09:58:34 +0100

    Removed banner during startup

--- application/src/main/java/com/compare/xsd/XsdCompareApplication.java
+++ application/src/main/java/com/compare/xsd/XsdCompareApplication.java
@@ -4,6 +4,7 @@ import com.compare.xsd.loaders.ViewLoader;
 import com.compare.xsd.managers.ViewManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,7 +14,9 @@ public class XsdCompareApplication extends Application {
     private static ApplicationContext APPLICATION_CONTEXT;
 
     public static void main(String[] args) {
-        APPLICATION_CONTEXT = SpringApplication.run(XsdCompareApplication.class, args);
+        SpringApplication application = new SpringApplication(XsdCompareApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        APPLICATION_CONTEXT = application.run(args);
         launch(args);
     }
 
